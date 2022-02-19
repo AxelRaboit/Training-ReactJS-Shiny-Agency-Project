1539553654993ccffc698a84e5f0fe0ac104b6a7
Axel Raboit <axel.raboit@gmail.com>
2022-02-19 02:06:31 +0100

    add logic to the survey and clg the result

--- shiny-agency/src/components/Results/index.jsx
+++ shiny-agency/src/components/Results/index.jsx
@@ -1,4 +1,12 @@
+import React, { useContext } from 'react';
+import { SurveyContext } from "../../utils/context";
+
+
 function Results() {
+
+  const { answers } = useContext(SurveyContext); 
+  console.log(answers);
+  
     return (
       <div>
         <h1>Résultats</h1>
--- shiny-agency/src/pages/Survey.jsx
+++ shiny-agency/src/pages/Survey.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import colors from '../utils/style/colors'
 import { Loader } from '../utils/style/Atoms'
+import { SurveyContext } from '../utils/context'
 
 const SurveyContainer = styled.div`
   display: flex;
@@ -30,6 +31,31 @@ const LinkWrapper = styled.div`
   }
 `
 
+const ReplyBox = styled.button`
+  border: none;
+  height: 100px;
+  width: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${colors.backgroundLight};
+  border-radius: 30px;
+  cursor: pointer;
+  box-shadow: ${(props) =>
+    props.isSelected ? `0px 0px 0px 2px ${colors.primary} inset` : 'none'};
+  &:first-child {
+    margin-right: 15px;
+  }
+  &:last-of-type {
+    margin-left: 15px;
+  }
+`
+
+const ReplyWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
 function Survey() {
   const { questionNumber } = useParams()
   const questionNumberInt = parseInt(questionNumber)
@@ -37,39 +63,29 @@ function Survey() {
   const nextQuestionNumber = questionNumberInt + 1
   const [surveyData, setSurveyData] = useState({})
   const [isDataLoading, setDataLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const { saveAnswers, answers } = useContext(SurveyContext)
 
-    //useEffect(() => {
-    // async function fetchData() {
-    //   try {
-    //     const response = await fetch(`http://localhost:8000/survey`)
-    //     const { surveyData } = await response.json()
-    //     setSurveyData(surveyData)
-    //   } catch (error) {
-    // console.log('===== error =====', error)
-    // setError(true)
-    //   }
-    // }
-    // })
+  function saveReply(answer) {
+    saveAnswers({ [questionNumber]: answer })
+  }
 
-    //or
+  //useEffect(() => {
+  // async function fetchData() {
+  //   try {
+  //     const response = await fetch(`http://localhost:8000/survey`)
+  //     const { surveyData } = await response.json()
+  //     setSurveyData(surveyData)
+  //   } catch (error) {
+  // console.log('===== error =====', error)
+  // setError(true)
+  //   }
+  // }
+  // })
 
-    /* useEffect(() => {
-      async function fetchSurvey() {
-        setDataLoading(true)
-        try {
-          const response = await fetch(`http://localhost:8000/survey`)
-          const { surveyData } = await response.json()
-          setSurveyData(surveyData)
-        } catch (err) {
-          console.log(err)
-        } finally {
-          setDataLoading(false)
-        }
-        fetchSurvey()
-      }
-    }, []); */
-
-  useEffect(() => {
+  //or
+    
+  /* useEffect(() => {
     setDataLoading(true)
     fetch(`http://localhost:8000/survey`).then((response) =>
       response.json().then(({ surveyData }) => {
@@ -77,7 +93,28 @@ function Survey() {
         setDataLoading(false)
       })
     )
+  }, []) */
+
+  useEffect(() => {
+    async function fetchSurvey() {
+      setDataLoading(true)
+      try {
+        const response = await fetch(`http://localhost:8000/survey`)
+        const { surveyData } = await response.json()
+        setSurveyData(surveyData)
+      } catch (err) {
+        console.log(err)
+        setError(true)
+      } finally {
+        setDataLoading(false)
+      }
+    }
+    fetchSurvey()
   }, [])
+
+  if (error) {
+    return <span>Oups il y a eu un problème</span>
+  }
 
   return (
     <SurveyContainer>
@@ -86,6 +123,24 @@ function Survey() {
         <Loader />
       ) : (
         <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
+      )}{ console.log(answers) }
+      {answers && (
+        
+        <ReplyWrapper>
+          <ReplyBox
+            onClick={() => saveReply(true)}
+            isSelected={answers[questionNumber] === true}
+            
+          >
+            Oui
+          </ReplyBox>
+          <ReplyBox
+            onClick={() => saveReply(false)}
+            isSelected={answers[questionNumber] === false}
+          >
+            Non
+          </ReplyBox>
+        </ReplyWrapper>
       )}
       <LinkWrapper>
         <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
